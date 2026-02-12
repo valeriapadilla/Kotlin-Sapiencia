886cda7f39c6bb8e272c4f8171f965d759041a77
valeriapadilla <vpadillahoyos@gmail.com>
2026-02-11 21:37:27 -0500

    clase 2 - 2026

--- src/academia/Estudiante.java
+++ src/academia/Estudiante.java
@@ -2,9 +2,39 @@ package academia;
 
 public class Estudiante {
 
-    public String estudiar(String materia){
+    protected String estudiar(String materia){
         String accion = "Estudiando " + materia;
         return accion;
+    }
+
+    public boolean verificarMateria(double[] notas){
+        int cantidadNotas = notas.length;
+
+        //vamos a calcular el promedio para saber si gana o pierde
+        double resultado = calcularNotaFinal(notas,cantidadNotas);
+
+        //si es > a 3.5 gana si es menor pierde
+        //if(si resultado es mayor o igual a 3.5){
+
+        if(resultado >= 3.5){
+            //si es verdadero entra aqui
+            System.out.println("Felicidades \n Gano la materia");
+            return true;
+        }else{
+            System.out.println("Repite materia, llorela");
+            return false;
+        }
+    }
+
+    private double calcularNotaFinal(double[] notas, int cantidad){
+        double suma = 0;
+
+        for(int nota=0; nota < cantidad; nota++){
+            //suma += notas[nota];
+            suma = suma + notas[nota];
+        }
+
+        return suma / cantidad;
     }
 
 }
